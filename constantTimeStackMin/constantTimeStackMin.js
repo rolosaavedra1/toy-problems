974a8c3333e7852205688bb52e5a51f5a807a28c
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2020-10-16 19:39:39 -0700

    Testing the tests, pushing only pseudo

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -17,24 +17,38 @@
 /**
   * Stack Class
   */
-  var Stack = function() {
+var Stack = function () {
+  this.storage = [];
+  this.minimums = [];
+};
 
-  // add an item to the top of the stack
-    this.push = function(value) {
-    };
+// add an item to the top of the stack
+this.prototype.push = function (value) {
+  //if the size is 0, just push it
+  //this.minimum.push(value);
+  //when any value is added, check if it is >= than this.minimum.
+  //if so, just push it
+  //if not, this.minimum.push(value)
+  //then push it.
+};
 
-  // remove an item from the top of the stack
-    this.pop = function() {
-    };
+// remove an item from the top of the stack
+this.prototype.pop = function () {
+  //if size is 0, don't do anything
+  //if this.storage(this.size() - 1) equals last item of minimums
+  // just remove it from top, then remove it from minimum
+  // if not, just remove it
+};
 
-  // return the number of items in the stack
-    this.size = function() {
-    };
-  
-  // return the minimum value in the stack
-    this.min = function() {
+// return the number of items in the stack
+this.prototype.size = function () {
+  return this.storage.length;
+};
 
-    };
+// return the minimum value in the stack
+this.prototype.min = function () {
+  return this.minimums.length;
 
-  };
+};
+
 
