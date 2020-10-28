14d59430697b01bbf7d9741014bce8e6b7f1f3b2
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2020-10-27 21:42:46 -0700

    final solution?

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,13 +12,15 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
- var allAnagrams = function(word) {
+ var allAnagrams = function(word, originalWord) {
   //create anagrams object, where we store every new anagram as a key.
   // if a new configuration doesn't exist, add a new key
   //it's going to have to be recursion, won't it
+
   if (word.length < 2) {
     return [word];
   } else {
+      var anagramsObj = {};
       var allAnswers = []
       //iterate through the word
       for (var i = 0; i < word.length; i++) {
@@ -26,12 +28,18 @@
         var letter = word[i];
         var shorterWord = word.substr(0, i) + word.substr(i + 1, word.length - 1);
         //then create an array of all anagrams for that shorter word
-        var shortwordArray = allAnagrams(shorterWord);
+        var shortwordArray = allAnagrams(shorterWord, word);
         for (var j = 0; j < shortwordArray.length; j++) {
           //then iterate through this array, adding the missing letter to every string
         allAnswers.push(letter + shortwordArray[j]);
-
+        //if at recursion depth of zero, add to array
+        if(!originalWord && !anagramsObj[letter + shortwordArray[j]]){
+          anagramsObj[letter + shortwordArray[j]] = letter + shortwordArray[j];
         }
+        }
+      }
+      if(!originalWord) {
+        return Object.keys(anagramsObj);
       }
       return allAnswers
   }
