d5a0efa800b05120e2211e28d79333d85413524c
rolosaavedra1 <rodrigo.saavedra@screendrive.io>
2020-10-27 20:23:50 -0700

    tentative solution (likely wrong)

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,6 +12,18 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
-  // Your code here.
+var allAnagrams = function(someString) {
+  //create anagrams object, where we store every new anagram as a key.
+  // if a new configuration doesn't exist, add a new key
+  //it's going to have to be recursion, won't it
+  //base case: 
+  var anagrams = [];
+  if (someString.length == 1) return someString;
+  for (var k in someString) {
+    var someString = someString[k];
+    allAnagrams(someString.join('').replace(someString, '').split('')).concat("").map(function(subtree) {
+      anagrams.push([someString].concat(subtree));
+    });
+  }
+  return anagrams.join('');
 };
